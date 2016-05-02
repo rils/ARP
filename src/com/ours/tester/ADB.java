0705de5871050e8ddbb826ad036de5cb0bc7fc9b
Mohammed Rilwan Moopan <rilwan@broadcom.com>
2016-05-03 00:27:04 +0900

    First version. OK-REC N PLAY TBD-resume timing. TBD-save/open only MEL files.

---
+++ src/com/ours/tester/ADB.java
@@ -0,0 +1,329 @@
+package com.ours.tester;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.android.ddmlib.AdbCommandRejectedException;
+import com.android.ddmlib.AndroidDebugBridge;
+import com.android.ddmlib.IDevice;
+import com.android.ddmlib.Log;
+import com.android.ddmlib.MultiLineReceiver;
+import com.android.ddmlib.ShellCommandUnresponsiveException;
+import com.android.ddmlib.SyncException;
+import com.android.ddmlib.TimeoutException;
+
+public class ADB {
+	public static final String LOG_TAG = "mytester";
+	public static String file_location;
+	public static boolean isStartNewLogFile = true;
+
+	private AndroidDebugBridge mAndroidDebugBridge;
+	public static IDevice[] mDevices;
+	public static IDevice mDevice;
+	public static FileOutputReceiver fReciever;
+	public static boolean isLogCancelled = false;
+	private static String myTesterEventScript_file = "/data/local/tmp/myTesterEventScript.mes";
+
+	private Utils mUtils;
+
+	public boolean initialize() {
+		boolean success = true;
+
+		String adbLocation;
+
+		adbLocation = System.getenv("ANDROID_HOME");
+		// Here, adbLocation may be android sdk directory
+		if (adbLocation != null) {
+			adbLocation += File.separator + "platform-tools";
+		}
+
+		if (success) {
+			if ((adbLocation != null) && (adbLocation.length() != 0)) {
+				adbLocation += File.separator + "adb";
+			} else {
+				adbLocation = "adb";
+			}
+			System.out.println("adb path is " + adbLocation);
+			AndroidDebugBridge.init(false);
+			mAndroidDebugBridge = AndroidDebugBridge.createBridge(adbLocation,
+					true);
+			if (mAndroidDebugBridge == null) {
+				success = false;
+			}
+		}
+
+		if (success) {
+
+			int count = 0;
+			while (!mAndroidDebugBridge.hasInitialDeviceList()) {
+
+				try {
+					Thread.sleep(100);
+					count++;
+				} catch (InterruptedException e) {
+				}
+				if (count > 100) {
+					success = false;
+					break;
+				}
+			}
+
+		}
+
+		if (!success) {
+			terminate();
+		}
+
+		return success;
+	}
+
+	public void terminate() {
+		AndroidDebugBridge.terminate();
+	}
+
+	public IDevice[] getDevices() {
+		IDevice[] devices = null;
+		if (mAndroidDebugBridge != null) {
+			devices = mAndroidDebugBridge.getDevices();
+		}
+		return devices;
+	}
+
+	public String[] adbDevices() {
+
+		mDevices = getDevices();
+
+		if (mDevices.length == 0) {
+			System.out.println("No connected devices!");
+			return null;
+		}
+		String[] list = new String[mDevices.length + 1];
+		if (mDevices != null) {
+			for (int i = 0; i < mDevices.length; i++) {
+				list[i] = mDevices[i].toString();
+			}
+			mDevice = mDevices[0];
+		}
+		list[mDevices.length] = "All Devices";
+		return list;
+	}
+
+	public String getDeviceName() {
+
+		return mDevice.getName();
+
+	}
+
+	public IDevice getMyDevice() {
+
+		return mDevice;
+
+	}
+
+	private class CollectingOutputReceiver extends MultiLineReceiver {
+
+		private StringBuffer mOutputBuffer = new StringBuffer();
+
+		public String getOutput() {
+			return mOutputBuffer.toString();
+		}
+
+		@Override
+		public void processNewLines(String[] lines) {
+			for (String line : lines) {
+				mOutputBuffer.append(line);
+				mOutputBuffer.append("\n");
+			}
+		}
+
+		public boolean isCancelled() {
+			return false;
+		}
+	}
+
+	public String shell(String command) throws IOException {
+
+		Log.i(LOG_TAG, String.format("adb shell %s", command));
+		System.out.println("adb shell " + command);
+		CollectingOutputReceiver receiver = new CollectingOutputReceiver();
+
+		try {
+			mDevice.executeShellCommand(command, receiver);
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AdbCommandRejectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ShellCommandUnresponsiveException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String output = receiver.getOutput();
+		Log.i(LOG_TAG, String.format("Result: %s", output));
+		return output;
+	}
+
+	public class FileOutputReceiver extends MultiLineReceiver {
+
+		private StringBuffer mOutputBuffer = new StringBuffer();
+
+		public void getOutputToFile() {
+
+			if (ADB.isStartNewLogFile) {
+				ADB.file_location = Paths.get("").toAbsolutePath().toString()
+						+ File.separator
+						+ new SimpleDateFormat("yyyyMMddhhmmss")
+								.format(new Date()) + "_events.mel";
+				System.out.println("New logging");
+			}
+			try {
+
+				FileWriter fw = new FileWriter(file_location, true);
+				fw.write(mOutputBuffer.toString());
+				fw.close();
+				System.out.println("Written to file " + file_location);
+			} catch (IOException e) {
+				System.out.println("Cant open file in this location: "
+						+ file_location);
+				e.printStackTrace();
+			}
+
+		}
+
+		@Override
+		public void processNewLines(String[] lines) {
+			for (String line : lines) {
+				mOutputBuffer.append(line);
+				mOutputBuffer.append("\n");
+			}
+		}
+
+		public boolean isCancelled() {
+			return isLogCancelled;
+		}
+	}
+
+	public FileOutputReceiver startlogGetEventToFile() {
+
+		isLogCancelled = false;
+		System.out.println("Start Logging...");
+		String command = "getevent -t";
+		FileOutputReceiver fReceiver = new FileOutputReceiver();
+		System.out.println("Before " + isStartNewLogFile);
+		try {
+			mDevice.executeShellCommand(command, fReceiver);
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AdbCommandRejectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ShellCommandUnresponsiveException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("From startloggin " + isStartNewLogFile);
+		fReceiver.getOutputToFile();
+		fReceiver.flush();
+		fReceiver.isCancelled();
+		fReceiver.done();
+		return fReceiver;
+	}
+
+	public String stoplogGetEventToFile() {
+
+		isLogCancelled = true;
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		isStartNewLogFile = true;
+		System.out.println("Stop: getevent logging done\n" + file_location);
+		return file_location;
+	}
+
+	public static boolean isStartNewLogFile() {
+		return isStartNewLogFile;
+	}
+
+	public void setStartNewLogFile(boolean isStartNewLogFile) {
+		ADB.isStartNewLogFile = isStartNewLogFile;
+	}
+
+	public String resumelogGetEventToFile() {
+
+		isStartNewLogFile = false;
+		System.out.println("Resume getevent logging " + file_location);
+		startlogGetEventToFile();
+		return file_location;
+	}
+
+	public String pauselogGetEventToFile() {
+
+		isLogCancelled = true;
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Pause: getevent logging done\n" + file_location);
+		return file_location;
+	}
+
+	public void runSendEventScript(String mes_script_location)
+			throws IOException {
+
+		String cmd = "sh " + mes_script_location;
+		shell(cmd);
+	}
+
+	public void convertPushPlayEventScript(String mel_file_location) {
+		String mes_script_file;
+		System.out.println("convertpushPlay" + mel_file_location);
+
+		try {
+			mes_script_file = new Utils()
+					.convertgetEventToSendEvent(mel_file_location);
+			adbPush(mes_script_file, myTesterEventScript_file);
+			runSendEventScript(myTesterEventScript_file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void adbPush(String src, String dst) {
+
+		System.out.println("adb push: " + src + "to " + dst);
+		try {
+			mDevice.pushFile(src, dst);
+		} catch (SyncException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AdbCommandRejectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/com/ours/tester/MainWindow.java
@@ -0,0 +1,504 @@
+package com.ours.tester;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileFilter;
+
+import com.android.ddmlib.IDevice;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MainWindow extends JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -770355362650071463L;
+	public static ADB mADB = new ADB();
+	private Utils mUtils;
+	public static IDevice[] mDevices;
+	public static IDevice mDevice;
+	private String devices[];
+
+	private static boolean isPlaying = false;
+	private static boolean isRecording = false;
+	private static boolean isPause = false;
+	private static boolean isResumed = false;
+
+	private static Thread RecordThread;
+	private static Thread PlayThread;
+
+	private String eventFilePath;
+	private String lastOpenPath;
+	private String eventFileToPlay;
+
+	private JButton btnRecord = new JButton("Rec");
+	private JButton btnStop = new JButton("Stop");
+	private JButton btnOpen = new JButton("Open");
+	private JButton btnSave = new JButton("Save");
+	private JComboBox comboBox;
+
+	private final JButton btnPlay = new JButton("Play");
+	private final JLabel lbl_action = new JLabel("");
+	private final JTextArea text_Curfile = new JTextArea("");
+	private final JSpinner spinner = new JSpinner();
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWindow() {
+
+		super("Record and Play");
+
+		mUtils = new Utils();
+
+		if (!mADB.initialize()) {
+			JOptionPane
+					.showMessageDialog(
+							this,
+							"Could not find adb, please install Android SDK and set adb path",
+							"Error", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		devices = mADB.adbDevices();
+		if (devices == null) {
+			JOptionPane
+					.showMessageDialog(
+							this,
+							"No Devices Conencted, Please Run again by conencting atleast once android device.",
+							"Error", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+
+		setTitle("Record N Play");
+		setSize(329, 187);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, 1.0, 1.0, 0.0, 0.0,
+				1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 1.0,
+				Double.MIN_VALUE };
+		getContentPane().setLayout(gridBagLayout);
+
+		JLabel lblSelectYourDevice = new JLabel("Select Your Device");
+		GridBagConstraints gbc_lblSelectYourDevice = new GridBagConstraints();
+		gbc_lblSelectYourDevice.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblSelectYourDevice.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSelectYourDevice.gridx = 0;
+		gbc_lblSelectYourDevice.gridy = 0;
+		getContentPane().add(lblSelectYourDevice, gbc_lblSelectYourDevice);
+
+		GridBagConstraints gbc_btnRecord = new GridBagConstraints();
+		gbc_btnRecord.fill = GridBagConstraints.HORIZONTAL;
+		btnRecord.setFont(new Font("Sans", Font.ROMAN_BASELINE, 14));
+		gbc_btnRecord.insets = new Insets(0, 0, 5, 0);
+		gbc_btnRecord.gridx = 9;
+		gbc_btnRecord.gridy = 0;
+		getContentPane().add(btnRecord, gbc_btnRecord);
+
+		comboBox = new JComboBox(devices);
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.gridwidth = 5;
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 0;
+		gbc_comboBox.gridy = 1;
+		getContentPane().add(comboBox, gbc_comboBox);
+
+		btnStop.setFont(new Font("Sans", Font.ROMAN_BASELINE, 14));
+		btnStop.setEnabled(false);
+		GridBagConstraints gbc_btnStop = new GridBagConstraints();
+		gbc_btnStop.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnStop.insets = new Insets(0, 0, 5, 0);
+		gbc_btnStop.gridx = 9;
+		gbc_btnStop.gridy = 1;
+		getContentPane().add(btnStop, gbc_btnStop);
+
+		GridBagConstraints gbc_btnSave = new GridBagConstraints();
+		gbc_btnSave.fill = GridBagConstraints.HORIZONTAL;
+		btnSave.setFont(new Font("Sans", Font.ROMAN_BASELINE, 14));
+		btnSave.setEnabled(false);
+		gbc_btnSave.insets = new Insets(0, 0, 5, 0);
+		gbc_btnSave.gridx = 9;
+		gbc_btnSave.gridy = 2;
+		getContentPane().add(btnSave, gbc_btnSave);
+
+		GridBagConstraints gbc_btnOpen = new GridBagConstraints();
+		gbc_btnOpen.fill = GridBagConstraints.HORIZONTAL;
+		btnOpen.setFont(new Font("Sans", Font.ROMAN_BASELINE, 14));
+		gbc_btnOpen.insets = new Insets(0, 0, 5, 0);
+		gbc_btnOpen.gridx = 9;
+		gbc_btnOpen.gridy = 3;
+		getContentPane().add(btnOpen, gbc_btnOpen);
+
+		GridBagConstraints gbc_lbl_nowPlaying = new GridBagConstraints();
+		gbc_lbl_nowPlaying.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lbl_nowPlaying.anchor = GridBagConstraints.SOUTH;
+		gbc_lbl_nowPlaying.insets = new Insets(0, 0, 5, 5);
+		gbc_lbl_nowPlaying.gridx = 0;
+		gbc_lbl_nowPlaying.gridy = 3;
+		lbl_action.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		getContentPane().add(lbl_action, gbc_lbl_nowPlaying);
+
+		GridBagConstraints gbc_lblCurfile = new GridBagConstraints();
+		gbc_lblCurfile.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblCurfile.anchor = GridBagConstraints.NORTH;
+		gbc_lblCurfile.insets = new Insets(0, 0, 0, 5);
+		gbc_lblCurfile.gridx = 0;
+		gbc_lblCurfile.gridy = 4;
+		text_Curfile.setEditable(false);
+		text_Curfile.setWrapStyleWord(true);
+		text_Curfile.setLineWrap(true);
+		text_Curfile.setOpaque(false);
+		text_Curfile.setFocusable(false);
+		text_Curfile.setBackground(UIManager.getColor("Label.background"));
+		text_Curfile.setFont(UIManager.getFont("Label.font"));
+		text_Curfile.setBorder(UIManager.getBorder("Label.border"));
+		text_Curfile.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		getContentPane().add(text_Curfile, gbc_lblCurfile);
+
+		GridBagConstraints gbc_spinner = new GridBagConstraints();
+		gbc_spinner.insets = new Insets(0, 0, 0, 5);
+		gbc_spinner.gridx = 8;
+		gbc_spinner.gridy = 4;
+
+		spinner.setEnabled(false);
+		spinner.setValue(1);
+		spinner.setToolTipText("Play count: " + spinner.getValue());
+		getContentPane().add(spinner, gbc_spinner);
+
+		GridBagConstraints gbc_btnPlay = new GridBagConstraints();
+		gbc_btnPlay.fill = GridBagConstraints.HORIZONTAL;
+		btnPlay.setFont(new Font("Sans", Font.ROMAN_BASELINE, 14));
+		gbc_btnPlay.gridx = 9;
+		gbc_btnPlay.gridy = 4;
+		getContentPane().add(btnPlay, gbc_btnPlay);
+		btnPlay.setEnabled(false);
+
+		btnRecord.addActionListener(this);
+		btnStop.addActionListener(this);
+		btnPlay.addActionListener(this);
+		btnSave.addActionListener(this);
+		btnOpen.addActionListener(this);
+		comboBox.addActionListener(this);
+
+		// pack();
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		Object source = event.getSource();
+		if (source instanceof JButton) {
+			JButton button = (JButton) source;
+			if (button == btnRecord) {
+				if (isPause) {
+					resumeRecording();
+				} else if (isRecording)
+					pauseRecording();
+				else
+					record();
+			} else if (button == btnStop)
+				stop();
+			else if (button == btnOpen)
+				openFile();
+			else if (button == btnSave)
+				saveFile();
+			else if (button == btnPlay)
+				play();
+
+		}
+		if (source instanceof JComboBox) {
+			JComboBox cb = (JComboBox) event.getSource();
+
+			if (cb.getSelectedItem().toString().contains("All"))
+				mADB.mDevice = mADB.mDevices[cb.getSelectedIndex() - 1];
+			else
+				mADB.mDevice = mADB.mDevices[cb.getSelectedIndex()];
+
+		}
+	}
+
+	private void record() {
+
+		isRecording = true;
+		isResumed = false;
+
+		RecordThread = new Thread(new Runnable() {
+
+			public void run() {
+
+				btnRecord.setText("Pause");
+				btnStop.setEnabled(true);
+				btnOpen.setEnabled(false);
+				btnPlay.setEnabled(false);
+				lbl_action.setText("Recording...");
+				mADB.startlogGetEventToFile();
+			}
+		});
+		System.out.println("out run Device: " + mADB.getDeviceName());
+		RecordThread.start();
+	}
+
+	private void stop() {
+
+		isPause = false;
+		isRecording = false;
+		isPlaying = false;
+		isResumed = false;
+
+		btnRecord.setText("Rec");
+		btnPlay.setEnabled(true);
+		btnRecord.setEnabled(true);
+		btnSave.setEnabled(true);
+		btnStop.setEnabled(false);
+		btnOpen.setEnabled(true);
+		spinner.setEnabled(true);
+		lbl_action.setText("Saved to:");
+		// RecordThread.interrupt();
+		eventFileToPlay = mADB.stoplogGetEventToFile();
+		text_Curfile.setText(eventFileToPlay);
+	}
+
+	private void pauseRecording() {
+
+		btnRecord.setText("Resume");
+		isPause = true;
+		isPlaying = false;
+		isRecording = false;
+		isResumed = false;
+
+		btnPlay.setEnabled(false);
+		btnRecord.setEnabled(true);
+		btnSave.setEnabled(true);
+		btnStop.setEnabled(true);
+
+		mADB.pauselogGetEventToFile();
+		// RecordThread.interrupt();
+	}
+
+	private void resumeRecording() {
+
+		isPause = false;
+		isPlaying = false;
+		isRecording = true;
+		isResumed = true;
+		btnRecord.setText("Pause");
+		btnSave.setEnabled(false);
+		// RecordThread.interrupt();
+		// mADB.updatelogGetEventToFile();
+		// RecordThread.start();
+		mADB.setStartNewLogFile(false);
+		record();
+	}
+
+	private void play() {
+
+		PlayThread = new Thread(new Runnable() {
+
+			public void run() {
+
+				isPlaying = true;
+				btnPlay.setEnabled(false);
+				btnRecord.setEnabled(false);
+				btnSave.setEnabled(false);
+				btnStop.setEnabled(true);
+				System.out.println(eventFileToPlay);
+				lbl_action.setText("Now Running:");
+				text_Curfile.setText(eventFileToPlay);
+				int count = (Integer) spinner.getValue();
+				for (int i = 1; i <= count; i++) {
+					lbl_action.setText("Now Running: x " + i);
+					mADB.convertPushPlayEventScript(eventFileToPlay);
+				}
+				System.out.println("End Play thread");
+				resetControls();
+				lbl_action.setText("");
+			}
+		});
+		System.out.println("Now Playing at: " + mADB.getDeviceName());
+		PlayThread.start();
+		// btnStop.setEnabled(false);
+	}
+
+	private void resetControls() {
+
+		isPause = false;
+		isRecording = false;
+		isPlaying = false;
+		isResumed = false;
+
+		btnRecord.setText("Rec");
+		btnPlay.setEnabled(true);
+		btnRecord.setEnabled(true);
+		btnSave.setEnabled(true);
+		btnStop.setEnabled(false);
+		btnOpen.setEnabled(true);
+		// RecordThread.interrupt();
+		// eventFileToPlay = mADB.stoplogGetEventToFile();
+	}
+
+	private void saveFile() {
+		JFileChooser fileChooser = null;
+
+		if (lastOpenPath != null && !lastOpenPath.equals("")) {
+			fileChooser = new JFileChooser(lastOpenPath);
+		} else {
+			fileChooser = new JFileChooser(Paths.get("").toAbsolutePath()
+					.toString());
+		}
+
+		FileFilter evtFilter = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "mTester Event file (*.MEL)";
+			}
+
+			@Override
+			public boolean accept(File file) {
+				if (file.isDirectory()) {
+					return true;
+				} else {
+					return file.getName().toLowerCase().endsWith(".mel");
+				}
+			}
+		};
+
+		fileChooser.setFileFilter(evtFilter);
+		fileChooser.setDialogTitle("Save Event File");
+		fileChooser.setAcceptAllFileFilterUsed(true);
+
+		int userChoice = fileChooser.showSaveDialog(this);
+		if (userChoice == JFileChooser.APPROVE_OPTION) {
+			eventFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+			Path dst = FileSystems.getDefault().getPath(eventFilePath);
+			Path src = FileSystems.getDefault().getPath(mADB.file_location);
+
+			try {
+				Files.move(src, dst, StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			System.out.println("saved to " + eventFilePath);
+			lbl_action.setText("File Saved As:");
+			text_Curfile.setText(eventFilePath);
+			mADB.file_location = eventFilePath;
+		}
+	}
+
+	private void openFile() {
+		JFileChooser fileChooser = null;
+
+		if (lastOpenPath != null && !lastOpenPath.equals("")) {
+			fileChooser = new JFileChooser(lastOpenPath);
+		} else {
+			fileChooser = new JFileChooser(Paths.get("").toAbsolutePath()
+					.toString());
+		}
+
+		FileFilter evtFilter = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "Event file (*.MEL)";
+			}
+
+			@Override
+			public boolean accept(File file) {
+				if (file.isDirectory()) {
+					return true;
+				} else {
+					return file.getName().toLowerCase().endsWith(".mel");
+				}
+			}
+		};
+
+		fileChooser.setFileFilter(evtFilter);
+		fileChooser.setDialogTitle("Open Event File");
+		fileChooser.setAcceptAllFileFilterUsed(true);
+
+		int userChoice = fileChooser.showOpenDialog(this);
+		if (userChoice == JFileChooser.APPROVE_OPTION) {
+			eventFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+			lastOpenPath = fileChooser.getSelectedFile().getParent();
+			System.out.println(eventFilePath);
+			eventFileToPlay = eventFilePath;
+		}
+		lbl_action.setText("Current File:");
+		text_Curfile.setText(fileChooser.getSelectedFile().getName());
+		btnPlay.setEnabled(true);
+		spinner.setEnabled(true);
+	}
+
+	public static void interrupt() throws IOException {
+
+		System.out.println("Interrupted");
+		int i = 0;
+		try {
+			Thread.sleep(1000);
+			System.out.println(i++);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+			System.out.println("NOW interupted");
+			if (isResumed) {
+
+				System.out
+						.println("NOW interupted because of resume, start getevent process here with append");
+			} else if (isPause) {
+				System.out
+						.println("NOW interupted because of pause, suspend getevent process here.");
+
+			} else {
+				System.out.println("!!!!");
+
+			}
+		}
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+				MainWindow ex = new MainWindow();
+				ex.setVisible(true);
+			}
+		});
+	}
+}
